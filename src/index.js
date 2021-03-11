a1375e9b3acc6fb15ae18db53780d3300f50c119
JunioResende <junioresende23@gmail.com>
2021-03-11 11:13:12 -0300

    listing extract

--- src/index.js
+++ src/index.js
@@ -30,6 +30,16 @@ app.post('/account', (request, response) => {
   return response.status(201).send()
 })
 
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params
+
+  const customer = customers.find(
+    customer => customer.cpf === cpf
+  )
+
+  return response.json(customer.statement)
+})
+
 app.listen(3333, () => {
   console.log('🚀Server started on port 3333!...')
 })

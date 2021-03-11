45f7d226b69f7461c650646d887b99c5f5ef8474
JunioResende <junioresende23@gmail.com>
2021-03-11 12:45:51 -0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,61 +1,58 @@
-const express = require('express')
-const { v4: uuidv4 } = require('uuid')
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const customers = []
+const customers = [];
 
-//Middleware
+// Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
-  const { cpf } = request.headers
+  const { cpf } = request.headers;
 
-  const customer = customers.find(
-    customer => customer.cpf === cpf
-  )
+  const customer = customers.find((customer) => customer.cpf === cpf);
 
   if (!customer) {
-    return response.status(400).json({ error: 'Customer not found' })
+    return response.status(400).json({ error: 'Customer not found' });
   }
 
-  request.customer =  customer
+  request.customer = customer;
 
-  return next()
+  return next();
 }
 
 app.post('/account', (request, response) => {
-  const { cpf, name} = request.body
+  const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some(
-    (customer) => customer.cpf === cpf
-  )
+    (customer) => customer.cpf === cpf,
+  );
 
   if (customerAlreadyExists) {
-    return response.status(400).json({ error: 'Customer already exists' })
+    return response.status(400).json({ error: 'Customer already exists' });
   }
 
-  const id = uuidv4()
+  const id = uuidv4();
 
   customers.push({
     id: uuidv4(),
     cpf,
     name,
-    statement: []
-  })
+    statement: [],
+  });
 
-  return response.status(201).send()
-})
+  return response.status(201).send();
+});
 
-//app.use(verifyIfExistsAccountCPF)
-
+// app.use(verifyIfExistsAccountCPF)
 
 app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
-  const { customer } = request
+  const { customer } = request;
 
-  return response.json(customer.statement)
-})
+  return response.json(customer.statement);
+});
 
 app.listen(3333, () => {
-  console.log('🚀Server started on port 3333!...')
-})
+  console.log('🚀Server started on port 3333!...');
+});

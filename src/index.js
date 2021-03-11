b86b46ce0726d99625c88db804eb7ac6c2c86aa5
JunioResende <junioresende23@gmail.com>
2021-03-11 14:40:11 -0300

    account update

--- src/index.js
+++ src/index.js
@@ -119,6 +119,19 @@ app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
   return response.json(statement);
 });
 
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  return response.status(201).send();
+});
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
 app.listen(3333, () => {
   console.log('🚀Server started on port 3333!...');
 });
